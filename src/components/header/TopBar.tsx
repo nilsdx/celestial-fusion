c3cde9657ea800b4475e7ce6783624fb013e935d
nilsdx <nils.durieux@gmail.com>
2026-01-28 16:12:19 +0100

    found the final project name, next security issue fix

--- src/components/header/TopBar.tsx
+++ src/components/header/TopBar.tsx
@@ -2,8 +2,8 @@ import SearchBar from "./SearchBar";
 
 const TopBar = () => {
     return (
-        <div className="flex bg-sky-950 p-8 justify-between text-slate-200">
-            <p>Linis's Archives</p>
+        <div className="flex items-center bg-sky-950 px-8 py-2 justify-between text-slate-200">
+            <p>Celestial Fusion</p>
             <SearchBar/>
         </div>
     )
