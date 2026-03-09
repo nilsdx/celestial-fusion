c0571aa9a8178ae301c85ce4450d28537f88797b
nilsdx <nils.durieux@gmail.com>
2026-03-09 16:36:25 +0100

    changed website description on main page

--- src/components/main/MainDescription.tsx
+++ src/components/main/MainDescription.tsx
@@ -9,12 +9,12 @@ const MainDescription = async () => {
 
     return (
         <>
-            <p>Welcome to</p>
+            <p className="text-2xl">Welcome to</p>
             <WebsiteLogo size={500}/>
             {isVapid ? (
-                <p>Thank you Vapid, very cool.</p>
+                <p className="text-3xl">Thank you Vapid, very cool.</p>
             ) : (
-                <p className="mx-32">This website contains informations about Destiny PSOBB's new features, from items to quests, as well as guides to help players in need, whether new or advanced.</p>
+                <p className="mx-32 text-xl">This website contains lots of knowledge about Destiny PSOBB's new features, from items to quests, as well as guides to help players in need. Whether you're a new or advanced player, a returnee or just someone curious about what this server can offer, you'll be sure to find useful knowledge regardless.</p>
             )}
         </>
     )
